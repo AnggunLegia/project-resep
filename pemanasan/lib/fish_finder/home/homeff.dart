f5260757e6878142a9320fedef2d05af0bd2167a
AnggunLegia <152955519+AnggunLegia@users.noreply.github.com>
2025-01-03 11:45:21 +0700

    add

--- pemanasan/lib/fish_finder/home/homeff.dart
+++ pemanasan/lib/fish_finder/home/homeff.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pemanasan/fish_finder/chart/chart.dart';
 import 'package:pemanasan/fish_finder/chart/chart2.dart';
 import 'package:pemanasan/fish_finder/chart/chart3.dart';
@@ -24,8 +25,11 @@ class _PageHomeFishState extends State<PageHomeFish> {
       appBar: AppBar(
         title: Center(
           child: Text(
-            "Fish Finder",
-            style: TextStyle(color: Colors.white),
+            "FishGo",
+            style:  GoogleFonts.amarante(
+                textStyle: Theme.of(context).textTheme.displayLarge,
+                fontSize: 15,
+                color: Colors.white),
           ),
         ),
         backgroundColor: Colors.blue[700],
@@ -40,7 +44,7 @@ class _PageHomeFishState extends State<PageHomeFish> {
                     context, MaterialPageRoute(builder: (BuildContext context) => chartfl()));
               },
               child: Container(
-                margin: EdgeInsets.only(top: 20, left: 30),
+                margin: EdgeInsets.only(top: 20, left: 5),
                 width: 300,
                 height: 150,
                 child: Center(child: Text("Cuaca")),
@@ -49,52 +53,54 @@ class _PageHomeFishState extends State<PageHomeFish> {
                     borderRadius: BorderRadius.circular(10)),
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (BuildContext context) => FishFinderApp()));
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 20, left: 30),
-                width: 300,
-                height: 150,
-                child: Center(child: Text("Fish Finder")),
-                decoration: BoxDecoration(
-                    border: Border.all(width: 2),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (BuildContext context) =>FishFinderApp2()));
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 20, left: 30),
-                width: 300,
-                height: 150,
-                child: Center(child: Text("Fish Finder 200")),
-                decoration: BoxDecoration(
-                    border: Border.all(width: 2),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (BuildContext context) => Chart03()));
-              },
-              child: Container(
-                margin: EdgeInsets.only(top: 20, left: 30),
-                width: 300,
-                height: 150,
-                child: Center(child: Text("Fish Finder 3")),
-                decoration: BoxDecoration(
-                    border: Border.all(width: 2),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (BuildContext context) => FishFinderApp()));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20, left:30),
+                    width: 150,
+                    height: 100,
+                    
+                      child: Row(
+                        children: [
+                          Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only( right: 10, top: 15, left: 10),
+                              child: Text("Fish Finder", style:  GoogleFonts.amarante(
+                              textStyle: Theme.of(context).textTheme.displayLarge,
+                              fontSize: 15,
+                              color: Colors.white),),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 60),
+                              width: 45,
+                              child: Image.asset("assets/images/ffis.png"),
+                            ),
+                          ],
+                                              ),
+                        ],
+                      ),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 8, 101, 193)
+                        ),
+                  ),
+                ),
+
+                 Container(
+              
+            )
+            
+              ],
             ),
             
+           
           ],
         ),
       ),
